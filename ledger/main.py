3d6f41d3cb0d75f16bc3574b77050aed06bebaa8
011000010110110101100100 <77757836+011000010110110101100100@users.noreply.github.com>
2021-03-01 08:48:15 -0500

    fix prototype xhr auth request

--- ledger/main.py
+++ ledger/main.py
@@ -68,8 +68,8 @@ def login():
     global sql
 
     if request.method == 'POST':
-        email = request.forms.get('email')
-        password = request.forms.get('password')
+        email = request.json.get('email')
+        password = request.json.get('password')
 
         sql.set_registrar()
 
